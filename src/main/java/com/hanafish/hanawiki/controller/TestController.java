8de1c9f5352d625dc36facb81351a18ba3f81bdf
hanafish <1106510024@qq.com>
2023-06-26 11:31:06 +0800

    2.6 feat: add hello interface test-test.http

--- src/main/java/com/hanafish/hanawiki/controller/TestController.java
+++ src/main/java/com/hanafish/hanawiki/controller/TestController.java
@@ -1,9 +1,7 @@
 package com.hanafish.hanawiki.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -13,5 +11,9 @@ public class TestController {
     @GetMapping("/hello")
     public String hello() {
         return "hello fish";
+    }
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "hello fish post," + name;
     }
 }
