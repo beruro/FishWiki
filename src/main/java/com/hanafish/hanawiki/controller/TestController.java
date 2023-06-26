74290d33eb15f7a34062c3b836279e5be2c1c7f8
hanafish <1106510024@qq.com>
2023-06-26 13:22:35 +0800

    2.8 feat: add Custom configuration items

--- src/main/java/com/hanafish/hanawiki/controller/TestController.java
+++ src/main/java/com/hanafish/hanawiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.hanafish.hanawiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
     @GetMapping("/hello")
     public String hello() {
-        return "hello fish";
+        return "hello fish"+testHello;
     }
     @PostMapping("/hello/post")
     public String helloPost(String name) {
