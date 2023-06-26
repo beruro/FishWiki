5fadb31a1636370e7b8ba8335be5e938be473f68
hanafish <1106510024@qq.com>
2023-06-26 09:29:04 +0800

    Feat: init

---
+++ src/main/java/com/hanafish/hanawiki/HanawikiApplication.java
@@ -0,0 +1,13 @@
+package com.hanafish.hanawiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanawikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanawikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanafish/hanawiki/HanawikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanafish.hanawiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanawikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
