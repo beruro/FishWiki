3cebc3a19a223d5d173ace3df7b993a8e9132894
hanafish <1106510024@qq.com>
2023-06-28 10:33:54 +0800

    4-7 feat: add the-header cpns

---
+++ web/src/components/the-header.vue
@@ -0,0 +1,30 @@
+<template>
+  <a-layout-header class="header">
+    <div class="logo" />
+    <a-menu
+        v-model:selectedKeys="selectedKeys1"
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="1">朝天门</a-menu-item>
+      <a-menu-item key="2">nav 2</a-menu-item>
+      <a-menu-item key="3">nav 3</a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+  type Props={
+    title:string
+  }
+
+  defineProps<Props>();
+
+</script>
+
+<style scoped>
+
+</style>
