a6ab8769e6f03d5284c81d0eb7038ff8d185757d
hanafish <1106510024@qq.com>
2023-06-28 21:08:27 +0800

    5-5 feat: Modify the baseUrl read configuration file for axios

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -109,7 +109,7 @@ const actions: Record<string, string>[] = [
   { type: 'MessageOutlined', text: '2' },
 ];
 onMounted(() => {
-  axios.get(process.env.VUE_APP_SERVER + '/ebook/list1').then((res) => {
+  axios.get('/ebook/list1').then((res) => {
     const data = res.data
     ebooks.value = data.content
     ebook1.books = data.content
