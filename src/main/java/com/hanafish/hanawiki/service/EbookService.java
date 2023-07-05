078e1adceec8fa9745502a55f0257b5562fcde23
hanafish <1106510024@qq.com>
2023-07-05 23:44:51 +0800

    6-4 feat: add PageHelper plugin, Change the Mapper log level to trace, print all SQL logs

--- src/main/java/com/hanafish/hanawiki/service/EbookService.java
+++ src/main/java/com/hanafish/hanawiki/service/EbookService.java
@@ -1,5 +1,7 @@
 package com.hanafish.hanawiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.hanafish.hanawiki.domain.Ebook;
 import com.hanafish.hanawiki.domain.EbookExample;
 import com.hanafish.hanawiki.mapper.EbookMapper;
@@ -7,6 +9,8 @@ import com.hanafish.hanawiki.req.EbookReq;
 import com.hanafish.hanawiki.resp.EbookResp;
 import com.hanafish.hanawiki.util.CopyUtil;
 import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -14,6 +18,8 @@ import java.util.List;
 
 @Service
 public class EbookService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
 
     @Resource
     private EbookMapper ebookMapper;
@@ -24,8 +30,12 @@ public class EbookService {
         if (!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
 //        List<EbookResp> respList = new ArrayList<>();
 //        for (Ebook ebook : ebookList) {
 //            EbookResp ebookResp = new EbookResp();
