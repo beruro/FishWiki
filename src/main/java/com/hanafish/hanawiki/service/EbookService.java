8f3375d0fe023208055be4bd02d8705329d3579a
hanafish <1106510024@qq.com>
2023-07-06 16:00:54 +0800

    6-10 feat: add delete ebook function

--- src/main/java/com/hanafish/hanawiki/service/EbookService.java
+++ src/main/java/com/hanafish/hanawiki/service/EbookService.java
@@ -71,4 +71,11 @@ public class EbookService {
             ebookMapper.updateByPrimaryKey(ebook);
         }
     }
+
+    /**
+     * 删除
+     */
+    public void delete (Long id) {
+        ebookMapper.deleteByPrimaryKey(id);
+    }
 }
--- web/src/views/admin/Admin-Ebook.vue
+++ web/src/views/admin/Admin-Ebook.vue
@@ -24,9 +24,16 @@
             <a-button type="primary" @click="edit(record)">
               编辑
             </a-button>
+            <a-popconfirm
+                title="删除后不可恢复，确认删除?"
+                ok-text="是"
+                cancel-text="否"
+                @confirm="handleDelete(record.id)"
+            >
               <a-button type="danger">
                 删除
               </a-button>
+            </a-popconfirm>
           </a-space>
         </template>
       </a-table>
@@ -174,12 +181,25 @@ import axios from 'axios';
     };
 
     /**
-     * 编辑
+     * 新增
      */
     const add = () => {
       modalVisible.value = true;
       ebook.value = {}
     };
+
+    const handleDelete = (id: number) => {
+      axios.delete("/ebook/delete/"+ id).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          //重新加载列表
+          handleQuery({
+            page: pagination.value.current,
+            size: pagination.value.pageSize
+          })
+        }
+      });
+    }
 
     onMounted(() => {
       handleQuery({
