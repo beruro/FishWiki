4e0fbe09525688750425615aab4b1e1e4bd6353f
hanafish <1106510024@qq.com>
2023-07-06 16:51:58 +0800

    6-11 feat: add parameter validation to the save interface

--- src/main/java/com/hanafish/hanawiki/req/EbookSaveReq.java
+++ src/main/java/com/hanafish/hanawiki/req/EbookSaveReq.java
@@ -1,8 +1,11 @@
 package com.hanafish.hanawiki.req;
 
+import jakarta.validation.constraints.NotNull;
+
 public class EbookSaveReq {
     private Long id;
 
+    @NotNull(message = "【名称】不能为空")
     private String name;
 
     private Long category1Id;
--- web/src/views/admin/Admin-Ebook.vue
+++ web/src/views/admin/Admin-Ebook.vue
@@ -120,7 +120,7 @@ import {message} from "ant-design-vue";
     const handleQuery = (params: any) => {
       loading.value = true;
       // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
-      // ebooks.value = [];
+      ebooks.value = [];
       axios.get("/ebook/list1",
           {
             params:
@@ -162,16 +162,17 @@ import {message} from "ant-design-vue";
     const handleModalOk = () => {
       modalLoading.value = true
       axios.post("/ebook/save",ebook.value).then((response) => {
+        modalLoading.value = false
         const data = response.data;
         if (data.success) {
           modalVisible.value = false;
-          modalLoading.value = false
-
           //重新加载列表
           handleQuery({
             page: pagination.value.current,
             size: pagination.value.pageSize
           })
+        }else {
+          message.error(data.message);
         }
       });
     }
