3355a38af87a9441af6fd0cc9066fa590d8b8142
hanafish <1106510024@qq.com>
2023-07-09 22:01:09 +0800

    8-4 feat: Using the recursive algorithm, the current target node and its descendant nodes in the attribute data are set to disabled, which solves the problem that the data is not updated when editing and saving and re-editing, and adds a "none" selection to the tree selection to add a new level node

--- web/src/views/admin/Admin-Doc.vue
+++ web/src/views/admin/Admin-Doc.vue
@@ -57,6 +57,18 @@
       <a-form-item label="名称">
         <a-input v-model:value="doc.name" />
       </a-form-item>
+      <a-form-item label="名称">
+        <a-tree-select
+            v-model:value="doc.parent"
+            style="width: 100%"
+            :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
+            :tree-data="treeSelectData"
+            placeholder="请选择父文档"
+            tree-default-expand-all
+            :replaceFields="{label: 'name', key: 'id', value: 'id'}"
+        >
+        </a-tree-select>
+      </a-form-item>
       <a-form-item label="父文档">
 <!--        <a-input v-model:value="doc.parent" />-->
         <a-select
@@ -87,6 +99,9 @@ import {Tool} from "@/util/tool";
     param.value = {};
     const docs = ref();
     const loading = ref(false);
+    // 因为树选择组件的属性状态，会随当前编辑的节点而变化，所以单独声明一个响应式变量
+    const treeSelectData = ref();
+    treeSelectData.value = [];
 
     const columns = [
       {
@@ -171,6 +186,13 @@ import {Tool} from "@/util/tool";
     const edit = (record: any) => {
       modalVisible.value = true;
       doc.value = Tool.copy(record);
+
+      // 不能选择当前节点及其所有子孙节点，作为父节点，会使树断开
+      treeSelectData.value = Tool.copy(level1.value);
+      setDisable(treeSelectData.value, record.id);
+
+      // 为选择树添加一个"无"
+      treeSelectData.value.unshift({id: 0, name: '无'});
     };
 
     /**
@@ -190,6 +212,37 @@ import {Tool} from "@/util/tool";
         }
       });
     }
+
+/**
+ * 将某节点及其子孙节点全部置为disabled
+ */
+const setDisable = (treeSelectData: any, id: any) => {
+  // console.log(treeSelectData, id);
+  // 遍历数组，即遍历某一层节点
+  for (let i = 0; i < treeSelectData.length; i++) {
+    const node = treeSelectData[i];
+    if (node.id === id) {
+      // 如果当前节点就是目标节点
+      console.log("disabled", node);
+      // 将目标节点设置为disabled
+      node.disabled = true;
+
+      // 遍历所有子节点，将所有子节点全部都加上disabled
+      const children = node.children;
+      if (Tool.isNotEmpty(children)) {
+        for (let j = 0; j < children.length; j++) {
+          setDisable(children, children[j].id)
+        }
+      }
+    } else {
+      // 如果当前节点不是目标节点，则到其子节点再找找看。
+      const children = node.children;
+      if (Tool.isNotEmpty(children)) {
+        setDisable(children, id);
+      }
+    }
+  }
+};
 
     onMounted(() => {
       handleQuery()
