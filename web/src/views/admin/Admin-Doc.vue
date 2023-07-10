d55a677b4759326febc3fc9e06212b6cf5c51262
hanafish <1106510024@qq.com>
2023-07-10 15:11:07 +0800

    8-11 feat: Add prompt for saving document success, edit and add when the time comes, clear the rich text box first

--- web/src/views/admin/Admin-Doc.vue
+++ web/src/views/admin/Admin-Doc.vue
@@ -207,7 +207,8 @@ import i18next from 'i18next'
         modalLoading.value = false
         const data = response.data;
         if (data.success) {
-          modalVisible.value = false;
+          // modalVisible.value = false;
+          message.success("保存成功！");
           //重新加载列表
           handleQuery();
         }else {
@@ -252,6 +253,8 @@ const handleQueryContent = () => {
      * 新增
      */
     const add = () => {
+      // // 清空富文本框
+      editor.txt.html("");
       modalVisible.value = true;
       doc.value = {
         ebookId: route.query.ebookId
