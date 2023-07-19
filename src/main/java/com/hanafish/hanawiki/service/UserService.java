e61cd8e195d3ce4739006338287d1b5b3fab452e
hanafish <1106510024@qq.com>
2023-07-19 16:33:22 +0800

    9-4 feat: Add verification that user names cannot be repeated, and add custom exceptions

--- src/main/java/com/hanafish/hanawiki/service/UserService.java
+++ src/main/java/com/hanafish/hanawiki/service/UserService.java
@@ -4,17 +4,20 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.hanafish.hanawiki.domain.User;
 import com.hanafish.hanawiki.domain.UserExample;
+import com.hanafish.hanawiki.exception.BusinessException;
+import com.hanafish.hanawiki.exception.BusinessExceptionCode;
 import com.hanafish.hanawiki.mapper.UserMapper;
 import com.hanafish.hanawiki.req.UserQueryReq;
 import com.hanafish.hanawiki.req.UserSaveReq;
-import com.hanafish.hanawiki.resp.UserQueryResp;
 import com.hanafish.hanawiki.resp.PageResq;
+import com.hanafish.hanawiki.resp.UserQueryResp;
 import com.hanafish.hanawiki.util.CopyUtil;
 import com.hanafish.hanawiki.util.SnowFlake;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 
 import java.util.List;
@@ -63,6 +66,16 @@ public class UserService {
     public void save(UserSaveReq req) {
         User user = CopyUtil.copy(req, User.class);
         if (ObjectUtils.isEmpty(req.getId())) {
+
+            User userDB = selectByLoginName(req.getLoginName());
+            if (ObjectUtils.isEmpty(userDB)) {
+                // 新增
+                user.setId(snowFlake.nextId());
+                userMapper.insert(user);
+            } else {
+                // 用户名已存在
+                throw new BusinessException(BusinessExceptionCode.USER_LOGIN_NAME_EXIST);
+            }
             // 新增
             user.setId(snowFlake.nextId());
             userMapper.insert(user);
@@ -77,5 +90,17 @@ public class UserService {
      */
     public void delete (Long id) {
         userMapper.deleteByPrimaryKey(id);
+    }
+
+    public User selectByLoginName(String LoginName) {
+        UserExample userExample = new UserExample();
+        UserExample.Criteria criteria = userExample.createCriteria();
+        criteria.andLoginNameEqualTo(LoginName);
+        List<User> userList = userMapper.selectByExample(userExample);
+        if (CollectionUtils.isEmpty(userList)) {
+            return null;
+        } else {
+            return userList.get(0);
+        }
     }
 }
--- web/src/views/admin/Admin-User.vue
+++ web/src/views/admin/Admin-User.vue
@@ -91,9 +91,9 @@
   import axios from 'axios';
   import { message } from 'ant-design-vue';
   import {Tool} from "@/util/tool";
-
-  declare let hexMd5: any;
-  declare let KEY: any;
+  //
+  // declare let hexMd5: any;
+  // declare let KEY: any;
 
 
       const param = ref();
@@ -172,7 +172,7 @@
       const handleModalOk = () => {
         modalLoading.value = true;
 
-        user.value.password = hexMd5(user.value.password + KEY);
+        // user.value.password = hexMd5(user.value.password + KEY);
 
         axios.post("/user/save", user.value).then((response) => {
           modalLoading.value = false;
@@ -228,7 +228,7 @@
       const handleResetModalOk = () => {
         resetModalLoading.value = true;
 
-        user.value.password = hexMd5(user.value.password + KEY);
+        // user.value.password = hexMd5(user.value.password + KEY);
 
         axios.post("/user/reset-password", user.value).then((response) => {
           resetModalLoading.value = false;
