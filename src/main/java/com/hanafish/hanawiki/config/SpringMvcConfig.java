4d9d1ee7a501f6cf1034ebffffaa2a0e7f2a38d8
hanafish <1106510024@qq.com>
2023-07-21 22:31:21 +0800

    9-11 feat: Added login blocking on the front and back ends

---
+++ src/main/java/com/hanafish/hanawiki/config/SpringMvcConfig.java
@@ -0,0 +1,34 @@
+package com.hanafish.hanawiki.config;
+
+import com.hanafish.hanawiki.interceptor.ActionInterceptor;
+import com.hanafish.hanawiki.interceptor.LoginInterceptor;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LoginInterceptor loginInterceptor;
+
+    @Resource
+    ActionInterceptor actionInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/vote/**",
+                        "/doc/find-content/**"
+//                        "/ebook-snapshot/**"
+                );
+    }
+}
