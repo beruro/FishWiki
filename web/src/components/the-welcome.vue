344e5df84b00f5dc5c38a355f2123dd7b9282120
hanafish <1106510024@qq.com>
2023-07-26 10:06:56 +0800

    11-8 feat: Add the welcome page and fixed bug

--- web/src/components/the-welcome.vue
+++ web/src/components/the-welcome.vue
@@ -125,16 +125,16 @@
           const data = response.data;
           if (data.success) {
             const statisticResp = data.content;
-            statistic.value.viewCount = statisticResp[1].viewCount;
-            statistic.value.voteCount = statisticResp[1].voteCount;
-            statistic.value.todayViewCount = statisticResp[1].viewIncrease;
-            statistic.value.todayVoteCount = statisticResp[1].voteIncrease;
+            statistic.value.viewCount = statisticResp[0].viewCount;
+            statistic.value.voteCount = statisticResp[0].voteCount;
+            statistic.value.todayViewCount = statisticResp[0].viewIncrease;
+            statistic.value.todayVoteCount = statisticResp[0].voteIncrease;
 
             // 按分钟计算当前时间点，占一天的百分比
             const now = new Date();
             const nowRate = (now.getHours() * 60 + now.getMinutes()) / (60 * 24);
             // console.log(nowRate)
-            statistic.value.todayViewIncrease = parseInt(String(statisticResp[1].viewIncrease / nowRate));
+            statistic.value.todayViewIncrease = parseInt(String(statisticResp[0].viewIncrease / nowRate));
             // todayViewIncreaseRate：今日预计增长率
             statistic.value.todayViewIncreaseRate = (statistic.value.todayViewIncrease - statisticResp[0].viewIncrease) / statisticResp[0].viewIncrease * 100;
             statistic.value.todayViewIncreaseRateAbs = Math.abs(statistic.value.todayViewIncreaseRate);
